d9ae84866654520faa27a16c23c8bbcadebded9b
Ann Alonzo <dotslashann@gmail.com>
2024-10-06 11:39:37 -0700

    base categories service unit tests

--- home-services-api/services/categories/categories.service_test.go
+++ home-services-api/services/categories/categories.service_test.go
@@ -31,9 +31,35 @@ func setupTestDB() *gorm.DB {
 	return db
 }
 
-func TestCategoriesServiceCreate(t *testing.T) {
+func getService() (categoriesService.CategoriesService, *gorm.DB) {
 	db := setupTestDB()
 	categoriesService := categoriesService.NewCategoriesService(db)
+
+	return categoriesService, db
+}
+
+func create(t *testing.T, service categoriesService.CategoriesService, names []string) []categories.Category {
+	categoriesList := make([]categories.Category, len(names))
+
+	for _, name := range names {
+		createCategoryDto := categoriesDto.CreateCategoryDto{
+			Name: name,
+		}
+
+		category, err := service.Create(createCategoryDto)
+
+		if err != nil {
+			t.Errorf("failed creating categories: %v", err)
+		}
+
+		categoriesList = append(categoriesList, category)
+	}
+
+	return categoriesList
+}
+
+func TestCategoriesServiceCreate(t *testing.T) {
+	categoriesService, db := getService()
 
 	createCategoryDto := categoriesDto.CreateCategoryDto{
 		Name: "mock category",
@@ -53,25 +79,15 @@ func TestCategoriesServiceCreate(t *testing.T) {
 }
 
 func TestCategoriesServiceFindAll(t *testing.T) {
-	db := setupTestDB()
-	categoriesService := categoriesService.NewCategoriesService(db)
+	categoriesService, _ := getService()
+
+	names := make([]string, 0, 5)
 
 	for i := 0; i < 5; i++ {
-		createCategoryDto := categoriesDto.CreateCategoryDto{
-			Name: fmt.Sprintf("mock category %v", i),
-		}
-
-		_, err := categoriesService.Create(createCategoryDto)
-
-		if err != nil {
-			t.Errorf("expected no error, but got: %v", err)
-		}
+		names = append(names, fmt.Sprintf("mock category %v", i))
 	}
 
-	var count int64
-	db.Model(&categories.Category{}).Count(&count)
-
-	assert.Equal(t, int64(5), count)
+	create(t, categoriesService, names)
 
 	categories, err := categoriesService.FindAll()
 
@@ -84,4 +100,70 @@ func TestCategoriesServiceFindAll(t *testing.T) {
 	for i, category := range categories {
 		assert.Equal(t, fmt.Sprintf("mock category %v", i), category.Name)
 	}
+}
+
+func TestCategoriesServiceFindOne(t *testing.T) {
+	categoriesService, _ := getService()
+
+	create(t, categoriesService, []string{"mock category"})
+
+	category, err := categoriesService.FindOne("1")
+
+	if err != nil {
+		t.Errorf("expected no error, but got: %v", err)
+	}
+
+	assert.Equal(t, uint(1), category.ID)
+	assert.Equal(t, "mock category", category.Name)
+}
+
+func TestCategoriesServiceFindByName(t *testing.T) {
+	categoriesService, _ := getService()
+
+	create(t, categoriesService, []string{"get by name"})
+
+	category, err := categoriesService.FindByName("get by name")
+
+	if err != nil {
+		t.Errorf("expected no error, but got: %v", err)
+	}
+
+	assert.Equal(t, uint(1), category.ID)
+	assert.Equal(t, "get by name", category.Name)
+}
+
+func TestCategoriesServiceUpdate(t *testing.T) {
+	categoriesService, _ := getService()
+
+	create(t, categoriesService, []string{"old name"})
+
+	category, _ := categoriesService.FindOne("1")
+
+	assert.Equal(t, "old name", category.Name)
+
+	category, err := categoriesService.Update("1", categoriesDto.UpdateCategoryDto{
+		Name: "new name",
+	})
+
+	assert.Nil(t, err)
+
+	category, _ = categoriesService.FindOne("1")
+
+	assert.Equal(t, "new name", category.Name)
+}
+
+func TestCategoriesServiceDelete(t *testing.T) {
+	categoriesService, _ := getService()
+
+	create(t, categoriesService, []string{"to be deleted"})
+
+	category, err := categoriesService.Delete("1")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "to be deleted", category.Name)
+
+	category, err = categoriesService.FindOne("1")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "record not found", err.Error())
 }
