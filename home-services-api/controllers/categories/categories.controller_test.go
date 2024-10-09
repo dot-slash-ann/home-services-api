7d4540df36c8638c660fba298d28dc59c96ea80c
Ann Alonzo <dotslashann@gmail.com>
2024-10-09 10:10:14 -0700

    [WIP] tests for controller

---
+++ home-services-api/controllers/categories/categories.controller_test.go
@@ -0,0 +1,205 @@
+package categories_test
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	categoriesController "github.com/dot-slash-ann/home-services-api/controllers/categories"
+	categoriesDto "github.com/dot-slash-ann/home-services-api/dtos/categories"
+	"github.com/dot-slash-ann/home-services-api/entities/categories"
+	"github.com/dot-slash-ann/home-services-api/lib/httpErrors"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockCategoriesService struct {
+	mock.Mock
+}
+
+func (m *MockCategoriesService) Create(categoryDto categoriesDto.CreateCategoryDto) (categories.Category, error) {
+	args := m.Called(categoryDto)
+
+	return args.Get(0).(categories.Category), args.Error(1)
+}
+
+func (m *MockCategoriesService) FindAll() ([]categories.Category, error) {
+	args := m.Called()
+
+	return args.Get(0).([]categories.Category), args.Error(1)
+}
+
+func (m *MockCategoriesService) FindOne(id string) (categories.Category, error) {
+	args := m.Called(id)
+
+	return args.Get(0).(categories.Category), args.Error(1)
+}
+
+func (m *MockCategoriesService) FindByName(name string) (categories.Category, error) {
+	args := m.Called(name)
+
+	return args.Get(0).(categories.Category), args.Error(1)
+}
+
+func (m *MockCategoriesService) Update(id string, updateCategoryDto categoriesDto.UpdateCategoryDto) (categories.Category, error) {
+	args := m.Called(id, updateCategoryDto)
+
+	return args.Get(0).(categories.Category), args.Error(1)
+}
+
+func (m *MockCategoriesService) Delete(id string) (categories.Category, error) {
+	args := m.Called(id)
+
+	return args.Get(0).(categories.Category), args.Error(1)
+}
+
+func TestCategoriesControllerCreate(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockService := new(MockCategoriesService)
+
+	mockService.On("Create", mock.AnythingOfType("CreateCategoryDto")).Return(categories.Category{}, nil)
+
+	controller := categoriesController.NewCategoriesController(mockService)
+
+	router := gin.Default()
+	router.POST("/test_create", controller.Create)
+
+	createRequestBody := `{"name":"mock category"}`
+
+	req, err := http.NewRequest(http.MethodPost, "/test_create", bytes.NewBuffer([]byte(createRequestBody)))
+	req.Header.Set("Content-Type", "application/json")
+
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusCreated, recorder.Code)
+
+	mockService.AssertNumberOfCalls(t, "Create", 1)
+	mockService.AssertCalled(t, "Create", categoriesDto.CreateCategoryDto{Name: "mock category"})
+	mockService.AssertExpectations(t)
+}
+
+func TestCategoriesControllerFindAll(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockService := new(MockCategoriesService)
+
+	mockService.On("FindAll").Return([]categories.Category{}, nil)
+
+	controller := categoriesController.NewCategoriesController(mockService)
+
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+
+	controller.FindAll(c)
+
+	assert.Len(t, c.Errors, 0)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	mockService.AssertNumberOfCalls(t, "FindAll", 1)
+	mockService.AssertCalled(t, "FindAll")
+	mockService.AssertExpectations(t)
+}
+
+func TestCategoriesControllerFindOne(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockService := new(MockCategoriesService)
+
+	mockService.On("FindOne", "1").Return(categories.Category{}, nil)
+
+	controller := categoriesController.NewCategoriesController(mockService)
+
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
+
+	controller.FindOne(c)
+
+	assert.Len(t, c.Errors, 0)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	mockService.AssertNumberOfCalls(t, "FindOne", 1)
+	mockService.AssertCalled(t, "FindOne", "1")
+	mockService.AssertExpectations(t)
+}
+
+func TestCategoriesControllerFindOneNotFound(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockService := new(MockCategoriesService)
+
+	mockService.On("FindOne", "1").Return(categories.Category{}, errors.New("record not found"))
+
+	controller := categoriesController.NewCategoriesController(mockService)
+
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
+
+	controller.FindOne(c)
+
+	errs := c.Errors
+
+	assert.Len(t, errs, 1)
+	assert.Equal(t, httpErrors.NotFoundError(errors.New("record not found"), nil).Error(), errs[0].Err.Error())
+
+	mockService.AssertNumberOfCalls(t, "FindOne", 1)
+	mockService.AssertCalled(t, "FindOne", "1")
+	mockService.AssertExpectations(t)
+}
+
+func TestCategoriesControllerUpdate(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockService := new(MockCategoriesService)
+
+	mockService.On("Update", "1", mock.AnythingOfType("UpdateCategoryDto")).Return(categories.Category{}, nil)
+
+	controller := categoriesController.NewCategoriesController(mockService)
+
+	router := gin.Default()
+	router.PATCH("/test_update/:id", controller.Update)
+
+	updateRequestBody := `{"name":"new name"}`
+
+	req, err := http.NewRequest(http.MethodPatch, "/test_update/1", bytes.NewBuffer([]byte(updateRequestBody)))
+	req.Header.Set("Content-Type", "application/json")
+
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	mockService.AssertNumberOfCalls(t, "Update", 1)
+	mockService.AssertCalled(t, "Update", "1", categoriesDto.UpdateCategoryDto{Name: "new name"})
+	mockService.AssertExpectations(t)
+}
+
+func TestCategoriesControllerDelete(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mockService := new(MockCategoriesService)
+
+	mockService.On("Delete", "1").Return(categories.Category{}, nil)
+
+	controller := categoriesController.NewCategoriesController(mockService)
+
+	router := gin.Default()
+	router.DELETE("/test_delete/:id", controller.Delete)
+
+	req, err := http.NewRequest(http.MethodDelete, "/test_delete/1", nil)
+
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	mockService.AssertNumberOfCalls(t, "Delete", 1)
+	mockService.AssertCalled(t, "Delete", "1")
+	mockService.AssertExpectations(t)
+}
